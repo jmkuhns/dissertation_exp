a5b52916e3246e887cfbde57e9a72d508c99fa93
jmkuhns <jmkuhns@uncg.edu>
2021-08-04 22:34:39 -0400

    Update grocery_price.js

--- grocery_price.js
+++ grocery_price.js
@@ -1,3 +1,16 @@
 /*
   Script for running the grocery price experiment!
 */
+
+
+// task desiderata
+// needs to be able to connect test and study stimuli across pattern comparison task
+// need to load in actual stimuli
+
+
+// study structure
+// instructions (how many pages?)
+// random order of stimuli (60 total)
+
+// how will the test stimuli be constructed?
+// use 16 counterbalances???
