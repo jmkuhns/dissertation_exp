01a96eaa029b7302f5f3dc9f1ab4881568f8fb02
jmkuhns <jmkuhns@uncg.edu>
2021-08-26 14:21:04 -0400

    adj adj

--- grocery_price.js
+++ grocery_price.js
@@ -523,11 +523,11 @@ var block = {
         var html = '<table><tr>' +
         '<td><img src='+
           jsPsych.timelineVariable("stimulus_1") +
-             ' style="width:150px;height:150px;display: block;margin: 125px 0px 75px 125px";>' +
+             ' style="width:150px;height:150px;display: block;margin: 50px 125px 50px 0px";>' +
              '</img></td>' +
         '<td><img src='+
         jsPsych.timelineVariable("stimulus_2") +
-        '  style="width:150px;height:150px;display: block;margin: 125px 125px 75px 0px";></img>'+
+        '  style="width:150px;height:150px;display: block;margin: 50px 0px 50px 125px";></img>'+
                    '</td></table>';
                      return html;
 
