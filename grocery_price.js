98140fc77dabbc52046e9ab05f4e525dcbc0846b
jmkuhns <jmkuhns@uncg.edu>
2021-10-10 19:21:37 -0400

    fixing spacing now

--- grocery_price.js
+++ grocery_price.js
@@ -1328,9 +1328,9 @@ var instr_p1_letter = {
 		choices: ["ArrowLeft", "ArrowRight"],
 		stimulus: function(){
 			var html = '<table><tr>' +
-			'<td style="padding:75px;"></td><td padding:style="padding:75px;"><p style="font-size:25px">'+
+			'<td style="padding:50px;"></td><td padding:style="padding:75px;"><p style="font-size:25px">'+
 				jsPsych.timelineVariable("stimulus_1") +
-					 '</p></td><td style="padding:75px;"></td><td style="padding:75px;"></td>' +
+					 '</p></td>'+
 			'<td style="padding:75px;"><p style="font-size:25px">'+
 			jsPsych.timelineVariable("stimulus_2") +
 								 '</p></td></tr></table>';
@@ -1951,8 +1951,8 @@ timeline.push(study_timeline);
 */
 
 // letter comparison task
-timeline.push(instr_p1_letter);
-timeline.push(instructions_letter);
+//timeline.push(instr_p1_letter);
+//timeline.push(instructions_letter);
 timeline.push(alt_practice_letter);
 timeline.push(interim_instructions_letter);
 timeline.push(trial_1_letter);
