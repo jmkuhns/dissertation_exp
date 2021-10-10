5b9aaad71d97a918af2e6245ef0a56c370872744
jmkuhns <jmkuhns@uncg.edu>
2021-10-10 19:11:35 -0400

    css style!

--- grocery_price.js
+++ grocery_price.js
@@ -1328,10 +1328,10 @@ var instr_p1_letter = {
 		choices: ["ArrowLeft", "ArrowRight"],
 		stimulus: function(){
 			var html = '<table><tr>' +
-			'<td padding:"900px"><p>" "</p></td><td padding:"900px"><p style="font-size:25px">'+
+			'<td style="padding:900px;"><p>" "</p></td><td padding:style="padding:900px;"><p style="font-size:25px">'+
 				jsPsych.timelineVariable("stimulus_1") +
-					 '</p></td><td padding:"900px"><p>" "</p></td><td padding:"900px"><p>" "</p></td>' +
-			'<td padding:"900px"><p style="font-size:25px">'+
+					 '</p></td><td style="padding:900px;"><p>" "</p></td><td style="padding:900px;"><p>" "</p></td>' +
+			'<td style="padding:900px;"><p style="font-size:25px">'+
 			jsPsych.timelineVariable("stimulus_2") +
 								 '</p></td></tr></table>';
 									 return html;
@@ -1382,11 +1382,11 @@ var instr_p1_letter = {
 				choices: ["ArrowLeft", "ArrowRight"],
 				stimulus: function(){
 					var html = '<table><tr>' +
-					'<td padding:"900px"><p>" "</p></td><td padding:"900px">'+
+					'<td style="padding:900px;"><p>" "</p></td><td style="padding:900px;">'+
           '<p style="font-size:25px">'+
 						jsPsych.timelineVariable("stimulus_1") +
-							 '</p></td><td padding:"900px"><p>" "</p></td><td padding:"900px"><p>" "</p></td>' +
-					'<td padding:"900px"><p style="font-size:25px">'+
+							 '</p></td><td style="padding:900px;"><p>" "</p></td><td style="padding:900px;"><p>" "</p></td>' +
+					'<td style="padding:900px;"><p style="font-size:25px">'+
 					jsPsych.timelineVariable("stimulus_2") +
 										 '</p></td></tr></table>';
 											 return html;
