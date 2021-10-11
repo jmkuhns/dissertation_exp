d296a9d186476399b2094a6a01798725b6c475f2
jmkuhns <jmkuhns@uncg.edu>
2021-10-11 13:48:31 -0400

    change to test intstr hope no break

--- grocery_price.js
+++ grocery_price.js
@@ -262,7 +262,7 @@ var test_instructions = {
   '<p style:"font-size:30px;text-align:center">Memory Test</p>' +
   '<div "font-size:25px>'+
   '<br><p>You will answer three multiple choice questions for each test pair. The response options for each question are written in capital letters below. For each test pair, you will be asked:</p>'+
-  '<ol style="text-align:center;>' +
+  '<ul style="text-align:center;>' +
   '<li>to judge whether the grocery-item and price were originally STUDIED together or were NOT STUDIED TOGETHER</li>'+
   '<li>to judge whether the grocery-item was originally paired with a price LESS THAN $6, MORE THAN $10, or to indicate that the the grocery-item in the test pair was NOT STUDIED</li>'+
   '<li>to judge whether the price in each test pair was on the study list, responding STUDIED if the price was on the study list, and responding NOT STUDIED if the price was not on the study list.</li></ol><br><p>You may press the Left arrow key to go back. You may press the Right arrow key to continue with the instructions.<br><br><br>p. 2/6</p></div>',
