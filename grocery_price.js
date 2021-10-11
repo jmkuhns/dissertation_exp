33e4d235ecb8b85c3329008052bbd57ed0e7f6b8
jmkuhns <jmkuhns@uncg.edu>
2021-10-11 12:29:29 -0400

    asdf

--- grocery_price.js
+++ grocery_price.js
@@ -1328,10 +1328,10 @@ var instr_p1_letter = {
 		choices: ["ArrowLeft", "ArrowRight"],
 		stimulus: function(){
 			var html = '<table><tr>' +
-			'<td style="padding:50px;"></td><td><p style="font-size:25px">'+
+			'<td style="padding:50px;"></td><td style ="width:150px"><p style="font-size:25px">'+
 				jsPsych.timelineVariable("stimulus_1") +
-					 '</p></td><td style="padding:75px;"></td>'+
-			'<td><p style="font-size:25px">'+
+					 '</p></td>'+
+			'<td style ="width:150px"><p style="font-size:25px">'+
 			jsPsych.timelineVariable("stimulus_2") +
 								 '</p></td><td "padding:75px;"></td></tr></table>';
 									 return html;
